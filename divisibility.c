bbfcebe47831d83cc418eed1348f7e658b7e3382
sm2702 <104062935+sm2702@users.noreply.github.com>
2022-04-23 18:05:09 +0530

    Update divisibility.c

--- divisibility.c
+++ divisibility.c
@@ -7,14 +7,17 @@ int main()
     printf("Enter any number: ");
     scanf("%d", &num);
     
-    if((num % 3
-        == 0) && (num % 5 == 0))
-    {
+    if((num % 3 == 0) && (num % 5 == 0)){
         printf("Number is divisible by 3 and 5");
     }
-    else
-    {
-        printf("Number is not divisible by 3 and 5");
+    else if((num % 3 == 0) && (num % 5 != 0)){
+        printf("Number is only divisible by 3");
+    }
+    else if((num % 5 == 0) && (num % 3 != 0)){
+        printf("Number is only divisible by 5");
+    }
+    else{
+        printf("Number is not divisible by 3 and 5 both");
     }
 
     return 0;
